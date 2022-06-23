8f0ef6d2b50fbdb1c34d805a5551e9f14b521d18
Seemo0 <medouallal08@gmail.com>
2022-06-23 17:20:12 +0100

    time updating

--- src/components/talk/TimeStatus.tsx
+++ src/components/talk/TimeStatus.tsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react";
 
 const TimeStatus = () => {
-    const [time, setTime] = useState<string>("00:00:00 p.m.");
+    const [time, setTime] = useState<Date>()
     const [awake, setAwake] = useState<boolean>(true);
 
-    function updateTime() {
-        let current = new Date().toLocaleString("ar-Mo", { timeZone: "UTC" });
-        setTime(`${current.slice(-11, -6)}${current.slice(-3, -1)}.M.`);
-        setTimeout(updateTime, 60 * 1000);
-
+    function updateTime() {    
+        setTime(new Date());
         if (new Date().getHours() < 7) setAwake(false);
     }
 
     useEffect(() => {
+        
         updateTime();
     }, []);
 
     return (
         <p className="text-black/50 dark:text-white/50 text-sm mb-10">
-            It's currently <span className="font-semibold text-black/60 dark:text-white/60">{time}</span> for me, so I'm
+            It's currently <span className="font-semibold text-black/60 dark:text-white/60">{time && time.toLocaleTimeString()}</span> for me, so I'm
             probably{" "}
             <span className="font-semibold text-black/60 dark:text-white/60">{awake ? "awake" : "sleeping"}</span>. I'll
             get back to you soon.
