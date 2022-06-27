2502e56b3c27f6527d6e7ea165e076b0602b69db
Seemo0 <medouallal08@gmail.com>
2022-06-27 17:21:22 +0100

    changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -69,7 +69,7 @@ const Nav = () => {
 
     return (
         <>
-            <motion.div className="hidden z-[999] fixed w-[90%] md:w-[50rem] xs:flex flex-row justify-between items-center px-4 py-2 mt-4 md:mt-6 rounded-md bg-white/60 dark:bg-[#12181d]/60 border border-slate-500/50 backdrop-blur-lg">
+            <motion.div className="hidden z-[999] fixed w-[90%] md:w-[50rem] xs:flex flex-row justify-between items-center px-4 py-2 mt-4 md:mt-6 rounded-md bg-white/60 dark:bg-[#c63445]/20 border border-slate-500/40 backdrop-blur-lg">
                 <div className="flex flex-row items-center justify-between gap-2">
                     <ThemeToggle />
                     <LandingButton name="Home" link="/" selected={router.pathname === "/"} />
